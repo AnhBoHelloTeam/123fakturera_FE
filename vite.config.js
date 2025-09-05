e4aef872437c980b011c91d848c2c06f3020972f
AnhBoHelloTeam <nhanhateku1@gmail.com>
2025-09-06 05:13:40 +0700

    Rebuild for SPA routing fix

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
+  base: '/',
   server: {
     port: 3000,
+  },
+  build: {
+    outDir: 'dist', // Đảm bảo thư mục đầu ra là 'dist'
+    assetsDir: 'assets', // Thư mục chứa các file tĩnh
   },
   esbuild: {
     loader: 'jsx',
